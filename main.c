b43985a80fd6ead1bd4e02e4d26b258b5deba673
ElbrayanATR <67125200+ElbrayanATR@users.noreply.github.com>
2020-06-19 18:57:39 -0300

    agregue comentario

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main()
     int estado=0;// variable de estado cargados 0 ninguno cargado 1 2 y 3 seria la cantida de cartones cargados
     // cargar en orden ejemplo si el estado es 0 no hay ninguno cargado
     // si el estado es 1 se cargo el carton 1 y el 2 y 3 estan vacios y asi sucesivamente;
-
+	//Angel se la come.
     printf("\n*****************************************************");
     printf("\n************** Bienvenido al Bingo ******************");
     printf("\n-----------------------------------------------------");
