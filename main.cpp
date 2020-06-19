eced9d8438b1065367637a9eecadc47927b05afd
InfernusTenshi <infernustenshi@gmail.com>
2020-06-19 13:51:06 -0300

    comentarios añadidos

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 	//Projecto 01
-	int bolilla[91]={0},numero=0;
+	int bolilla[91]={0},numero=0; // bolilla es un vector de control unicamente los numeros se generan en numero
 	srand(time(NULL));
 	for(int I=0;I<20;I++)
 	{
@@ -16,7 +16,7 @@ int main()
 		printf("numero nuevo no repetido = %d\n",numero);
 		bolilla[numero] = -1;
 	}
-	else if(bolilla[numero] == -1)
+	else if(bolilla[numero] == -1) // elemento solo de prueba en futuro tiene que ser eliminado
 	{
 		printf("numero repetido error ----> %d\n",numero);
 	}
