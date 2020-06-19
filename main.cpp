5a33eafa560da11d9e4d58ace727b32652d119e6
InfernusTenshi <infernustenshi@gmail.com>
2020-06-19 14:14:09 -0300

    se añadieron la funcion de crear carton aleatorio y vaciar Vectores, eliminado mensajes de prueba

--- main.cpp
+++ main.cpp
@@ -2,25 +2,62 @@
 #include <conio.h>
 #include <time.h>
 #include <stdlib.h>
-
+void generarAleatorio(int Cart[5][3],int disp[91]);
+void Vaciar(int Vec[91]); // mas seguro que asignar Vec[91] = {0};
 int main()
 {
 	//Projecto 01
-	int bolilla[91]={0},numero=0; // bolilla es un vector de control unicamente los numeros se generan en numero
-	srand(time(NULL));
+	srand(time(NULL)); // movido de lugar
+	int bolilla[91],numero=0; // bolilla es un vector de control unicamente los numeros se generan en numero
+	int numerosDisp[91];
+	int cartonPrueba[5][3]={0};
+	Vaciar(bolilla);
+	Vaciar(numerosDisp);
+//	generarAleatorio(cartonPrueba,numerosDisp);
 	for(int I=0;I<20;I++)
 	{
 	numero=rand()%91;
 	if(bolilla[numero] == 0)
 	{
-		printf("numero nuevo no repetido = %d\n",numero);
+		printf("Salio el Numero ---> %d\n",numero);
 		bolilla[numero] = -1;
-	}
-	else if(bolilla[numero] == -1) // elemento solo de prueba en futuro tiene que ser eliminado
-	{
-		printf("numero repetido error ----> %d\n",numero);
 	}
 	}
 
-	
+	return 0;
+}
+void generarAleatorio(int Cart[5][3],int disp[91])
+{
+	int numero=0;
+	for(int C=0;C<3;C++)
+	{
+		for(int F=0;F<5;F++)
+		{
+			numero=rand()%91;
+			while(disp == 0)
+			{
+				numero=rand()%91;	
+			}
+			if(disp[numero] == 0)
+			{
+				Cart[F][C]=numero;
+				printf(" %d ",numero);
+			}
+		}
+		printf(" \n");
+	}
+}
+void Vaciar(int Vec[91])
+{
+	for(int I=0;I<91;I++)
+	{
+		if(I == 0)
+		{
+			Vec[I] = -1; // el numero 0 no se utiliza en el bingo
+		}
+		else
+		{
+			Vec[I] = 0; // vaciado para dejar numeros como disponibles
+		}
+	}
 }
