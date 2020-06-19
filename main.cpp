215bb3a2f73c1ab5d761c2ee45ea6a67f7db6492
InfernusTenshi <infernustenshi@gmail.com>
2020-06-19 13:49:11 -0300

    generador de aleatorios sin repetirse

--- main.cpp
+++ main.cpp
@@ -6,4 +6,21 @@
 int main()
 {
 	//Projecto 01
+	int bolilla[91]={0},numero=0;
+	srand(time(NULL));
+	for(int I=0;I<20;I++)
+	{
+	numero=rand()%91;
+	if(bolilla[numero] == 0)
+	{
+		printf("numero nuevo no repetido = %d\n",numero);
+		bolilla[numero] = -1;
+	}
+	else if(bolilla[numero] == -1)
+	{
+		printf("numero repetido error ----> %d\n",numero);
+	}
+	}
+
+	
 }
