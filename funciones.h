54c6a02f9ee847bd178e2bb5a2401993fab60693
NatanielOtero <natanielotero@gmail.com>
2020-07-28 14:44:34 -0300

    Limpieza

--- funciones.h
+++ funciones.h
@@ -14,20 +14,6 @@ void RegistrarJugador(char nombreJugador[8],char apellidoJugador[8], int dniJuga
 int CantidadCartones();
 void Vaciar(int Vec[91]);
 void cartonesMaquina(int cantidad);
-/*
 void Escribir(int punt,int dni,char nomb[10],char ape[10]);
-void MostrarCarton(int cantidad,int opcion);
-void colorear(int Matr[5][3],int Dispo[91]);
-void Normal(int Cart[5][3],int Comp[91]);
-void MenuPrincipal(int estado,int limite);
-
-void ComenzarJuego(int limite);
-void Aleatorio(int Cart[5][3],int disp[91]);
-void Vaciar(int Vec[91]); // mas seguro que asignar Vec[91] = {0};
-void CargarCarton();
-
-void GenerarCartonMaquina(int estado,int limite);
-void Escribir(int punt,int dni,char nomb[10],char ape[10]);
-void Leer();*/
-
+void Leer();
 #endif // FUNCIONES_H_INCLUDED
--- main.c
+++ main.c
@@ -118,21 +118,9 @@ int main()
 	
 		   
 		   
-	}
-		    
+	}		    
    
 
-   /* int estado=0;// variable de estado cargados 0 ninguno cargado 1 2 y 3 seria la cantida de cartones cargados
-    // cargar en orden ejemplo si el estado es 0 no hay ninguno cargado
-    // si el estado es 1 se cargo el carton 1 y el 2 y 3 estan vacios y asi sucesivamente;
-
-    printf("\n*****************************************************");
-    printf("\n************** Bienvenido al Bingo ******************");
-    printf("\n-----------------------------------------------------");
-    cantidadCartones = CantidadCartones();
-    if(cantidadCartones != 0)
-    {
-        MenuPrincipal(estado,cantidadCartones);
-    }*/
+   
     return 0;
 }
